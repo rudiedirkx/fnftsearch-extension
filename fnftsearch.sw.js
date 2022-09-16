a6bf6a98665c860089b024e6aff471995aa35748
Rudie Dirkx <github@hotblocks.nl>
2022-09-16 22:34:00 +0200

    runtime error logs+

--- fnftsearch.sw.js
+++ fnftsearch.sw.js
@@ -6,9 +6,9 @@ chrome.runtime.onInstalled.addListener(function() {
 		"id": 'fnftsearch',
 		"contexts": ['selection'],
 	});
-	console.log(chrome.runtime.lastError);
+	console.log(1, chrome.runtime.lastError);
 });
-console.log(chrome.runtime.lastError);
+console.log(2, chrome.runtime.lastError);
 
 chrome.contextMenus.onClicked.addListener(function(info, tab) {
 	const url = FNFTSEARCH_FNFT_URL + '?search=' + encodeURIComponent(info.selectionText.trim());
@@ -17,6 +17,6 @@ chrome.contextMenus.onClicked.addListener(function(info, tab) {
 		active: true,
 		index: tab.index + 1,
 	});
-	console.log(chrome.runtime.lastError);
+	console.log(3, chrome.runtime.lastError);
 });
-console.log(chrome.runtime.lastError);
+console.log(4, chrome.runtime.lastError);
